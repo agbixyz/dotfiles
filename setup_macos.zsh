1919906f448edcec339e36e920f3bf15fee8131a
agbixyz <106268432+agbixyz@users.noreply.github.com>
2022-06-04 05:36:16 +0900

    Tweak dock autohide with non-GUI settings

--- setup_macos.zsh
+++ setup_macos.zsh
@@ -11,7 +11,9 @@ defaults write com.apple.finder NewWindowTargetPath -string "file://$HOME/.dotfi
 
 # System Preferences > Dock
 defaults write com.apple.dock magnification -bool true
-defaults write com.apple.dock tilesize -float 35
-defaults write com.apple.dock largesize -float 55
+defaults write com.apple.dock tilesize -int 35
+defaults write com.apple.dock largesize -int 55
 defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-time-modifier -float 0.25
+defaults write com.apple.dock autohide-delay -float 0.1
 killall Dock

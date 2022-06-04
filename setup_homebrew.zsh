0a5cc3089aca959f912dd2dcd595a3c813f268a6
agbixyz <106268432+agbixyz@users.noreply.github.com>
2022-06-05 07:15:17 +0900

    Fix insecure directories after Homebrew install

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -22,3 +22,9 @@ brew bundle --verbose
 echo "Enter superuser (sudo) password to accept Xcode license"
 sudo xcodebuild -license accept
 sudo xcodebuild -runFirstLaunch
+
+# This works to solve the Insecure Directories issue:
+# compaudit | xargs chmod go-w
+# But this is from the Homebrew site, though `-R` was needed:
+# https://docs.brew.sh/Shell-Completion#configuring-completions-in-zsh
+chmod -R go-w "$(brew --prefix)/share"
